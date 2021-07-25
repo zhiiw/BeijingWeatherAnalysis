966485f7b1c61cc92f00b065ee08f9e8bb11f2e3
zhiiw <1302593554@qq.com>
2021-07-25 09:50:39 +0800

    front end initialization

--- frontend/weatheranalysis/src/pages/calendar.vue
+++ frontend/weatheranalysis/src/pages/calendar.vue
@@ -1,6 +1,8 @@
 <template>
 <q-page>
+
   <div id="q-app" style="min-height: 100vh;">
+
     <q-select v-model="model" :options="options" label="Standard" model-value="Beijing"></q-select>
 
     <div class="q-pa-md">
@@ -9,6 +11,7 @@
         landscape
        model-value='2021/07/24'></q-date>
     </div>
+
   </div>
 </q-page>
 </template>
@@ -19,13 +22,47 @@ export default {
   name: 'calendar',
   data(){
     return{
+      confirm: false,
       model:"Beijing",
       options:["shanghai","shenzhen"],
       date:'2021/07/24'
     }
   },
   methods:{
-    trunToIndex()
+    trunToIndex(){
+      let _this = this
+
+      this.$q.dialog({
+        title: 'Jump?',
+        message: 'Are you sure to turn to this page'
+      }).onOk(() => {
+        this.$axios.post('http://127.0.0.1:8001/api/login',
+          {
+            username: this.username,
+            password: this.password,
+
+
+          }).then(function (response) {
+          let res = response.data
+
+          sessionStorage.setItem('loggedIn', _this.username)
+          sessionStorage.setItem('user_id', res.user_id)
+          if(res.is_admin === 1)
+            sessionStorage.setItem('role', '1')
+          this.$router.push('/index')
+
+        }).catch(function (error) {
+          console.log(error)
+        })
+        this.routes.push("/index")
+      }).onCancel(() => {
+
+      }).onDismiss(() => {
+
+      })
+      return
+
+    }
   }
 }
 </script>
